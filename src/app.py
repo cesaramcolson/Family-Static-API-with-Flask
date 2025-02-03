3938a1d5f3baa2959dedbdf774a69a9accbf91d1
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-02-03 23:54:17 +0000

    get and post methods created

--- src/app.py
+++ src/app.py
@@ -14,6 +14,28 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+initial_data = [
+    {
+        "first_name": "John",
+        "last_name": "Jackson",
+        "age": 33,
+        "lucky_numbers": [7, 13, 22]
+    },
+    {
+        "first_name": "Jane",
+        "last_name": "Jackson",
+        "age": 35,
+        "lucky_numbers": [10, 14, 3]
+    },
+    {
+        "first_name": "Jimmy",
+        "last_name": "Jackson",
+        "age": 5,
+        "lucky_numbers": [1]
+    }
+]
+for members_data in initial_data:
+    jackson_family.add_member(members_data)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -27,16 +49,18 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
-
-    return jsonify(response_body), 200
+@app.route('/members', methods=['POST'])
+def add_member():
+    body = request.get_json()
+    if body is None:
+        return jsonify({"msg:" "Bad request"}), 400
+    jackson_family.add_member(body)
+    return jsonify({"msg:" "Member added succesfully"}), 200
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
